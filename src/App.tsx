9b430b5192a462cf1ba1cb40835721be4b872e45
Jutr0 <micwikski@gmail.com>
2021-08-24 17:33:57 +0200

    ToDoList

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [toDos, setToDos] = useState<string[]>([])
+  const [newToDo, setNewToDo] = useState('')
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setNewToDo('')
+    setToDos([...toDos, newToDo]);
+  }
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewToDo(e.target.value)
+  }
+  const handleDelete = (e: React.MouseEvent<HTMLButtonElement>, id: number) => {
+    e.preventDefault()
+    setToDos([...toDos.slice(0, id), ...toDos.slice(id + 1, toDos.length)]);
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main id="container">
+
+      <section className="toDoList">
+        <h1>To Do List</h1>
+        <div className="input">
+          <form >
+            <input type="text" value={newToDo} onChange={handleChange} />
+            <button onClick={handleClick}>Add</button>
+          </form>
+        </div>
+        <div>
+          {toDos.map((step, id) =>
+            <div key={step + id} className="toDo"><span>{step}</span><button onClick={(e) => handleDelete(e, id)}>x</button></div>
+          )}
+        </div>
+      </section>
+
+    </main >
   );
 }
 
